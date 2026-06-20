ea5da194dfd32fd56281afcef665581937c05d26
SharifPoetra <sharifpoetra1@gmail.com>
2026-06-20 23:53:48 +0800

    feat: regitser the kick endpoint

--- server/src/modules/member/member.routes.ts
+++ server/src/modules/member/member.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
-import { addMemberHandler } from './member.controller.js';
+import { addMemberHandler, kickMemberHandler } from './member.controller.js';
 
 export const memberRoutes = async (app: FastifyInstance) => {
   app.post('/invite', { preHandler: [app.authenticate] }, addMemberHandler);
+  app.delete('/kick', { preHandler: [app.authenticate] }, kickMemberHandler);
 };
