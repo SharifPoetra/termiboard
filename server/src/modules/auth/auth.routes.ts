6a12ce3719ca4e854727d688d05b2379200a1e48
Sharif <43358217+SharifPoetra@users.noreply.github.com>
2026-06-28 18:44:34 -0700

    feat: add endpoint forupdating profile detailes (#18)

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,9 +1,8 @@
 import { FastifyInstance } from 'fastify';
-import { registerHandler, loginHandler } from './auth.controller.ts';
+import { registerHandler, loginHandler, updateProfileHandler } from './auth.controller.ts';
 
 export const authRoutes = async (app: FastifyInstance) => {
-  // Define POST /api/auth/register route
   app.post('/register', registerHandler);
-  // Define POST /api/auth/login route
   app.post('/login', loginHandler);
+  app.patch('/profile', { preHandler: app.authenticate }, updateProfileHandler);
 };
